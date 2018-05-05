07f1cefbf20322cde5a833607c100fbdcb6106d0
Misha <tymish1@gamail.com>
2018-05-05 17:09:01 +0300

    added messages tunel for call background and content script

--- background.js
+++ background.js
@@ -1,5 +1,15 @@
-'use strict';
-
 chrome.runtime.onInstalled.addListener(details => {
     // console.log('Extension was installed!', details.previousVersion);
+    sendingMessage();
 });
+
+chrome.browserAction.onClicked.addListener(() => {
+    sendingMessage();
+});
+
+function sendingMessage() {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {greeting: 'start_killer'}, () => {
+      });
+    });
+  }
--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-console.log('killer advertising content script');
+chrome.runtime.onMessage.addListener(async(request) => {
+    if (request.greeting === 'start_killer') {
+        init();
+    }
+});
+
+function init() {
+    console.log('killer advertising content script');
+}
